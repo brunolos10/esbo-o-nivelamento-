bd1a2d08eda4f0be47019337991f96436bfb5cbf
brunolos10 <160243976+brunolos10@users.noreply.github.com>
2024-03-11 08:43:03 -0300

    salvar

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup () {}
+   createCanvas (800, 800); 
+   redground("red");
+
+
+
+function drew () {
+ stroke("black");
+fill ("blue")
+
+if(mouseIsPressed)
+   rect(mouseX, mouseY , 17, 17) 
+  }
+}
+
